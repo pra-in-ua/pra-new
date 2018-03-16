01497f04bed04b9d11d12df98c2bad5ebac24814
Jaroslaw Goszowski <goszowski@gmail.com>
2018-03-16 14:21:35 +0200

    Адаптивне меню

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -6,6 +6,12 @@
 		<a class="navbar-brand" href="#">
 			<img src="/assets/images/global/logo.png" width="80" alt="">
 		</a>
+
+		<div class="ml-auto mr-4 d-xs-block d-lg-none">
+			<a href="#" class="btn btn-sm btn-outline-success btn-outline-has-bg text-white mr-3"><i class="fas fa-life-ring"></i> <span class="d-none d-sm-inline">Підтримати проект</span></a>
+			<a href="#" class="btn btn-sm btn-outline-warning btn-outline-has-bg text-white"><i class="fas fa-sign-in-alt"></i> Увійти</a>
+		</div>
+
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
@@ -29,7 +35,7 @@
 				</li>
 			</ul>
 
-			<div class="ml-4">
+			<div class="ml-4 d-none d-lg-block">
 				<a href="#" class="btn btn-sm btn-outline-success btn-outline-has-bg text-white mr-3">Підтримати проект</a>
 				<a href="#" class="btn btn-sm btn-outline-warning btn-outline-has-bg text-white">Увійти</a>
 			</div>
--- views/layouts/resources.blade.php
+++ views/layouts/resources.blade.php
@@ -2,8 +2,12 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Pra.in.ua</title>
 
+	<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/solid.js" integrity="sha384-+Ga2s7YBbhOD6nie0DzrZpJes+b2K1xkpKxTFFcx59QmVPaSA8c7pycsNaFwUK6l" crossorigin="anonymous"></script>
+	<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/fontawesome.js" integrity="sha384-7ox8Q2yzO/uWircfojVuCQOZl+ZZBg2D2J5nkpLqzH1HY0C1dHlTKIbpRz/LG23c" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="/assets/bootstrap-4.0.0/dist/css/bootstrap.min.css">
 </head>
 <body>
