0a9a483af5cf8451ae1c4fa013688e1c7c725054
Jaroslaw Goszowski <goszowski@gmail.com>
2018-03-19 17:49:02 +0200

    PACE

--- views/layouts/resources.blade.php
+++ views/layouts/resources.blade.php
@@ -15,5 +15,6 @@
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="/assets/bootstrap-4.0.0/dist/js/bootstrap.min.js"></script>
+	<script src="/assets/node_modules/pace-js/pace.min.js"></script>
 </body>
 </html>
